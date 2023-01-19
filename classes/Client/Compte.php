58bef204d288641e7f7a68fb8db06daca111baa7
TetianaKfr <kosorukova.tetiana.a@gmail.com>
2023-01-19 16:27:35 +0100

    final namespace

--- classes/Client/Compte.php
+++ classes/Client/Compte.php
@@ -1,10 +1,15 @@
 <?php 
 	namespace App\Client;
 
-	class Compte
+class Compte
+{
+	private $nom;
+	private $prenom;
+
+	public function __construct(string $nomClient, string $prenomClient)
 	{
-
+		$this->nom = $nomClient;
+		$this->prenom = $prenomClient;
 	}
-	
-
+}
  ?>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+use App\Client\Compte as CompteClient;
+use App\Banque\{CompteCourant, CompteEpargne};
+
 require_once 'classes/Banque/Compte.php';
 require_once 'classes/Banque/CompteCourant.php';
 require_once 'classes/Banque/CompteEpargne.php';
@@ -8,16 +11,19 @@ require_once 'classes/Client/Compte.php';
 
 // On instancie le compte
 // On instancie sans préciser le découvert
-$compte1 = new App\Banque\CompteCourant('Benoit', 2000);
+$compte1 = new CompteCourant('Benoit', 2000);
 $compte1->retirer(4000);
 // On instancie en précisant le découvert
-$compte2 = new App\Banque\CompteCourant('Tetiana', 100);
+$compte2 = new CompteCourant('Tetiana', 100);
 var_dump($compte1);
 echo "<br>","<br>";
 var_dump($compte2);
 echo "<br>", "<br>";
-$compteEpragne = new App\Banque\CompteEpargne('Robert', 200, 1);
+$compteEpragne = new CompteEpargne('Robert', 200, 1);
 var_dump($compteEpragne);
+echo "<br>", "<br>";
+$client = new CompteClient("Benoit", "Lafevre");
+var_dump($client);
 
 
 
